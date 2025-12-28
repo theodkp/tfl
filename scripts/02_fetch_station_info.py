d1e5b3076edee07a19d4ae734d478f2bbb834455
theodkp <theo.paraskeva123@gmail.com>
2025-12-28 00:52:21 +0000

    init fetch station script

---
+++ scripts/02_fetch_station_info.py
@@ -0,0 +1,59 @@
+import pandas as pd
+from src.tfl_api import api
+from pathlib import Path
+import logging
+
+
+URL = "https://api.tfl.gov.uk/StopPoint/Mode/tube"
+
+
+
+logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(message)s')
+
+
+def fetch_stations(key:str) -> pd.DataFrame:
+    ''' Fetches info about each station, we save id as well as lines the station serves '''
+
+
+    try:
+        res = api(URL, key)
+
+        rows = []
+
+        for sp in res["stopPoints"]:
+            if sp["stopType"] != "NaptanMetroStation":
+                continue
+
+            station = sp["stationNaptan"]
+
+            for line in sp.get("lines", []):
+                rows.append({
+                    "station_naptan": station,
+                    "line_id": line["id"],
+                })
+
+        return pd.DataFrame(rows).drop_duplicates()
+    
+    except Exception as e:
+        logging.error(f"Failed to fetch stations: {e}")
+        raise
+
+    
+if __name__ == "__main__":
+
+
+    key = "123"
+
+    df = fetch_stations(key)
+
+    OUT_DIR = Path(f"data/raw/stations/")
+
+    OUT_DIR.mkdir(parents=True, exist_ok=True)
+
+    OUT_FILE = OUT_DIR / "line_station.parquet"
+
+    df.to_parquet(OUT_FILE, index=False)
+
+    logging.info(f"Successfully saved {len(df)} rows to {OUT_FILE}")
+
+
